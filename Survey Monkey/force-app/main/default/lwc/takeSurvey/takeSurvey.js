acbc2ac867b4c2d4aab3b0da7aab60fa74cb204e
Andy Dugan <94930377+RealAndyDugan@users.noreply.github.com>
2022-02-16 18:59:09 -0600

    Feature 2

--- Survey Monkey/force-app/main/default/lwc/takeSurvey/takeSurvey.js
+++ Survey Monkey/force-app/main/default/lwc/takeSurvey/takeSurvey.js
@@ -1,7 +1,28 @@
-import { LightningElement } from 'lwc';
-
+import { LightningElement, wire, track, api } from 'lwc';
+import { CurrentPageReference } from 'lightning/navigation';
 import logo from '@salesforce/resourceUrl/surveyMonkey_logo';
+import getQuestionList from '@salesforce/apex/getQuestions.getQuestionList';
+import getTemplateList from '@salesforce/apex/getTemplate.getTemplateList';
+import { getRecord } from 'lightning/uiRecordApi';
 
 export default class TakeSurvey extends LightningElement {
     surveyMonkeyLogo = logo;
+    //Get template param
+    @api recordId;
+    @track recordId;
+    @wire(CurrentPageReference)
+    getStateParameters(currentPageReference) {
+        if (currentPageReference) {
+            const urlValue = currentPageReference.state.c__recordId;
+            if (urlValue) {
+                this.recordId = urlValue;
+            } else {
+                this.recordId = `URL Value was not set`;
+            }
+        }
+    }
+
+    @wire(getTemplateList, { Id: '$recordId' })
+    template;
+    
 }

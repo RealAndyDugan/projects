24854dbeeefbe1f1c144b8f460100fafad9b5ddd
Andy Dugan <94930377+RealAndyDugan@users.noreply.github.com>
2022-02-21 17:59:35 -0600

    View My Responses
    Still need to fix date

--- Survey Monkey/force-app/main/default/lwc/viewMyResponses/viewMyResponses.js
+++ Survey Monkey/force-app/main/default/lwc/viewMyResponses/viewMyResponses.js
@@ -1,38 +1,50 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
 import Id from '@salesforce/user/Id';
 import logo from '@salesforce/resourceUrl/surveyMonkey_logo';
+import getSurveyList from '@salesforce/apex/getSurveys.getSurveyList';
 
 export default class ViewMyResponses extends LightningElement {
     surveyMonkeyLogo = logo;
     userId = Id;
     @track customerSurveyOpen = false;
-    @track productReviewOpen = false;
 
     get customerSurveyIconName() {
-        return 'utility:' + (this.customerSurveyOpen ? 'chevrondown' : 'chevronright');
-    }
-
-    get productReviewIconName() {
-        return 'utility:' + (this.productReviewOpen ? 'chevrondown' : 'chevronright');
+        return 'utility:' + 'chevronright';
     }
 
     get customerSurveyClassList() {
-        return this.customerSurveyOpen ? 'slds-section__content slds-p-horizontal_medium' : 'slds-hide';
+        return 'slds-hide';
     }
 
-    get productReviewClassList() {
-        return this.productReviewOpen ? 'slds-section__content slds-p-horizontal_medium' : 'slds-hide';
-    }
+    //get customerSurveyClassList() {
+    //    return this.customerSurveyOpen ? 'slds-section__content slds-p-horizontal_medium' : 'slds-hide';
+    //}
 
-    customerSurveyButtonClick() {
+
+    customerSurveyButtonClick(event) {
+        // VVV
+        let index = event.currentTarget.dataset.index;
+        let survey = this.surveys.data[index];
+        // ^^^
+        let htmlIndex = '[data-survey-id="' + survey.Id + '"]';
+        if(this.template.querySelector(htmlIndex).classList.contains('slds-hide')) {
+            this.template.querySelector(htmlIndex).classList.remove("slds-hide");
+            this.template.querySelector(htmlIndex).classList.add(["slds-section__content", "slds-p-horizontal_medium"]);
+        } else if (this.template.querySelector(htmlIndex).classList.contains('slds-section__content,slds-p-horizontal_medium')){
+            this.template.querySelector(htmlIndex).classList.remove(["slds-section__content", "slds-p-horizontal_medium"]);
+            this.template.querySelector(htmlIndex).classList.add("slds-hide");
+        }
+        let buttonIndex = '[data-button="' + survey.Id + '"]';
+        if(this.template.querySelector(buttonIndex).iconName == 'utility:chevronright') {
+            this.template.querySelector(buttonIndex).iconName = "utility:chevrondown";
+        } else if (this.template.querySelector(buttonIndex).iconName == 'utility:chevrondown'){
+            this.template.querySelector(buttonIndex).iconName = "utility:chevronright";
+        }
         this.customerSurveyOpen = !this.customerSurveyOpen;
-    }
-
-    productReviewButtonClick() {
-        this.productReviewOpen = !this.productReviewOpen;
     }
 
     //*****************************************//
 
-
+    @wire(getSurveyList, { Id: '$userId' })
+    surveys;
 }
